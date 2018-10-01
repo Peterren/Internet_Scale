7597722e78f969b0c9925e41a63068078486ca4f
Yincheng Ren <peterren0205@gmail.com>
2018-10-01 18:31:29 -0400

    Update urls.py

--- p1/products/urls.py
+++ p1/products/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('', list_products, name='list_products'),
     path('new', create_product, name='create_product'),
     path('update/<int:id>', update_product, name='update_product'),
-    path('delete/<int:id', delete_product, name='delete_product'),
+    path('delete/<int:id>', delete_product, name='delete_product'),
 ]
