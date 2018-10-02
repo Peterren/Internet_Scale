19c008e161bea01a504c238630122de20f330f95
JamesJMH <jh7zr@virginia.edu>
2018-10-02 17:21:03 -0400

    access drivers list

--- p1/products/models.py
+++ p1/products/models.py
@@ -5,7 +5,6 @@ class Product(models.Model):
     description = models.CharField(max_length=200)
     price = models.DecimalField(max_digits=9, decimal_places=2)
     quantity = models.IntegerField()
-    def __str__(self):
-        return self.question_text
+
 
 
