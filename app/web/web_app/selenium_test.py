aa6995dbe0dc27f952e02e6029a3134f70570229
JamesJMH <jh7zr@virginia.edu>
2018-12-04 18:47:02 -0500

    more tests

--- app/web/web_app/selenium_test.py
+++ app/web/web_app/selenium_test.py
@@ -3,23 +3,42 @@ from django.core.urlresolvers import reverse
 import selenium
 from selenium import webdriver
 import requests
+from .views import search_drivers
 
-Class SeleniumTests:
+Class SeleniumTests(TestCase):
     
-    def test_drivers_list_html:
-        driver=webdriver.chrome()
+    def setUp(self):
+        driver=webdriver.Chrome()
+    
+    def test_drivers_list_page(self):
         driver.get("localhost:8000//list_drivers")
         self.assertEquals(driver.find_element_by_css_selector('title').text, 'Drivers')
         self.assertTrue(len(driver.find_elements_by_css_selector('ul')) > 0)
     
-    def test_products_list_html:
-        driver=webdriver.chrome()
+    def test_create_driver_link(self):
+        driver.get("localhost:8000//list_drivers")
+        drivers=driver.find_elements_by_css_selector('a')
+        new_driver_button=drivers[-1]
+        new_driver_button.click()
+        self.assertEquals(driver.get_element_by_css_selector('title').text, "Driver Information)
+    
+    def test_update_driver_link(self):
+        driver.get("localhost:8000//list_drivers")
+        drivers=driver.find_elements_by_css_selector('a')
+        update_button=drivers[0]
+        update_button.click()
+        self.assertEquals(driver.get_element_by_css_selector('title').text, "Driver Information)     
+    
+    def test_404_page(self):
+        driver.get("localhost:8000//list_drive")
+        self.assertEquals(driver.get_element_by_xpath('//h1[./a]').text, 'warning')
+    
+    def test_products_list_html(self):
         driver.get("localhost:8000//list_products")
         self.assertEquals(driver.find_element_by_css_selector('title').text, 'Product List')
         self.assertTrue(len(driver.find_elements_by_class_name)>0)
     
-    def test_create_user_page:
-        driver=webdriver.chrome()
+    def test_create_user_page(self):
         driver.get("localhost:8000//register")
         self.assertEquals(driver.find_element_by_css_selector('h1').text, 'Register')
         self.assertEquals(driver.find_element_by_class_name("mdl-button mdl-js-button mdl-js-ripple-effect"), 'Login to existing account') 
