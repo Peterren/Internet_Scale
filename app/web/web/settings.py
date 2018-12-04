692f3ec73bbb2abfbc7530ec5a654cde5503fd3a
yanmx <my2pk@virginia.edu>
2018-12-04 13:25:35 -0500

    Update settings.py
    add redis setting

--- app/web/web/settings.py
+++ app/web/web/settings.py
@@ -136,6 +136,17 @@ COMPRESS_PRECOMPILERS = (
     ('text/x-css', 'django_libsass.SassCompiler'),
 )
 
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+            "PASSWORD": "password",
+        }
+    }
+}
+
 # Sass
 SASS_PROCESSOR_ENABLED = True
 SASS_PROCESSOR_INCLUDE_DIRS = [
