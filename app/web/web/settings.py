458fef4b97f6b4a2819ae02617d2c0bd7780861d
yanmx <my2pk@virginia.edu>
2018-12-04 13:36:59 -0500

    Update settings.py
    add cache setting

--- app/web/web/settings.py
+++ app/web/web/settings.py
@@ -49,6 +49,8 @@ MIDDLEWARE = [
     'web_app.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.cache.UpdateCacheMiddleware',
+   'django.middleware.cache.FetchFromCacheMiddleware',
 ]
 
 ROOT_URLCONF = 'web.urls'
