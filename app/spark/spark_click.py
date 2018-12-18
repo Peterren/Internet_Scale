24198f5e75fde5dcb0a193231fbfd07c3037af22
Yincheng Ren <yr5ka@virginia.edu>
2018-12-18 10:33:20 +0800

    recommendation sys

--- app/spark/spark_click.py
+++ app/spark/spark_click.py
@@ -20,7 +20,11 @@ user_of_item=click_of_users_pair.map(lambda x: (x[1],x[0]))
 # Transform into ((item1, item2), count of distinct users who co-clicked (item1, item2)
 # Filter out any results where less than 3 users co-clicked the same pair of items
 user_of_item_cnt = user_of_item.groupByKey.map(lambda itemuser: (itemuser,len(itemuser))).filter(lambda cnt: cnt[1]>=3)
-data=dataset.map(lambda word: (word,len(word))).filter(lambda t : t[1] >=6) 
+
+# Create a recommendation list which has (item1, recommended items for item1)
+recommend_items1 = user_of_item_cnt.map(lambda x: (x[0][0]:x[0][1]))
+recommend_items2 = user_of_item_cnt.map(lambda x: (x[0][1]:x[0][0]))
+recommend_item_final = recommend_items1.union(recommend_items2).groupByKey()
 
 pages = pairs.map(lambda pair: (pair[1], 1))      # re-layout the data to ignore the user id
 count = pages.reduceByKey(lambda x,y: int(x)+int(y))        # shuffle the data so that each key is only on one worker
